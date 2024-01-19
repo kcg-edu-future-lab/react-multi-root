68cc91b9043633db7eadd14a19186e8b7fdd48a6
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2024-01-19 13:45:53 +0900

    initial commit.

---
+++ src/App2.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App2.css';
+
+function App2() {
+  return (
+    <div className="App2">
+      <header className="App2-header">
+        <p>App2です。</p>
+      </header>
+    </div>
+  );
+}
+
+export default App2;
